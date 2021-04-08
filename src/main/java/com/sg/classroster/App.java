d48c7c602b29743564d2548b699eed23b0ba986a
zhilinIPD19 <lin.zhilin0@gmail.com>
2021-04-08 13:34:01 -0400

    Initial commit

---
+++ src/main/java/com/sg/classroster/App.java
@@ -0,0 +1,20 @@
+package com.sg.classroster;
+
+import com.sg.classroster.controller.ClassRosterController;
+import com.sg.classroster.dao.ClassRosterDao;
+import com.sg.classroster.dao.ClassRosterDaoException;
+import com.sg.classroster.dao.ClassRosterDaoFileImpl;
+import com.sg.classroster.ui.ClassRosterView;
+import com.sg.classroster.ui.UserIO;
+import com.sg.classroster.ui.UserIOConsoleImpl;
+
+public class App {
+    public static void main(String[] args) throws ClassRosterDaoException {
+        UserIO myIo = new UserIOConsoleImpl();
+        ClassRosterView myView = new ClassRosterView(myIo);
+        ClassRosterDao myDao = new ClassRosterDaoFileImpl();
+        ClassRosterController controller =
+                new ClassRosterController(myDao, myView);
+        controller.run();
+    }
+}
---
+++ src/main/java/com/sg/classroster/controller/ClassRosterController.java
@@ -0,0 +1,95 @@
+package com.sg.classroster.controller;
+
+import com.sg.classroster.dao.ClassRosterDao;
+import com.sg.classroster.dao.ClassRosterDaoException;
+import com.sg.classroster.dto.Student;
+import com.sg.classroster.ui.ClassRosterView;
+import com.sg.classroster.ui.UserIO;
+import com.sg.classroster.ui.UserIOConsoleImpl;
+
+import java.util.List;
+
+public class ClassRosterController {
+    private UserIO io = new UserIOConsoleImpl();
+    private ClassRosterView view;
+    private ClassRosterDao dao;
+
+    public ClassRosterController(ClassRosterDao dao, ClassRosterView view) {
+        this.dao = dao;
+        this.view = view;
+    }
+
+    public void run() throws ClassRosterDaoException {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        try {
+            while (keepGoing) {
+
+                menuSelection = getMenuSelection();
+
+                switch (menuSelection) {
+                    case 1:
+                        listStudents();
+                        break;
+                    case 2:
+                        createStudent();
+                        break;
+                    case 3:
+                        viewStudent();
+                        break;
+                    case 4:
+                        removeStudent();
+                        break;
+                    case 5:
+                        keepGoing = false;
+                        break;
+                    default:
+                        unknownCommand();
+                }
+
+            }
+            exitMessage();
+        } catch (ClassRosterDaoException e) {
+            view.displayErrorMessage(e.getMessage());
+        }
+    }
+
+    private int getMenuSelection() {
+        return view.printMenuAndGetSelection();
+    }
+
+    private void createStudent() throws ClassRosterDaoException {
+        view.displayCreateStudentBanner();
+        Student newStudent = view.getNewStudentInfo();
+        dao.addStudent(newStudent.getStudentId(), newStudent);
+        view.displayCreateSuccessBanner();
+    }
+
+    private void listStudents() throws ClassRosterDaoException {
+        view.displayDisplayAllBanner();
+        List<Student> studentList = dao.getAllStudents();
+        view.displayStudentList(studentList);
+    }
+
+    private void viewStudent() throws ClassRosterDaoException {
+        view.displayDisplayStudentBanner();
+        String studentId = view.getStudentIdChoice();
+        Student student = dao.getStudent(studentId);
+        view.displayStudent(student);
+    }
+
+    private void removeStudent() throws ClassRosterDaoException {
+        view.displayRemoveStudentBanner();
+        String studentId = view.getStudentIdChoice();
+        Student removedStudent = dao.removeStudent(studentId);
+        view.displayRemoveResult(removedStudent);
+    }
+
+    private void unknownCommand() {
+        view.displayUnknownCommandBanner();
+    }
+
+    private void exitMessage() {
+        view.displayExitBanner();
+    }
+}
